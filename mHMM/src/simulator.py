81a24decc7e8e89866a94c9726aca947cb73d830
Clint Os <dwayneqlint1@gmail.com>
2025-11-06 00:50:20 +0300

    Fix(src): fix import module path bug

--- mHMM/src/simulator.py
+++ mHMM/src/simulator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from mHMM.models.emissions import EmissionModel
-from mHMM.models.transitions import TransitionModel
+from mHMM.src.emissions import EmissionModel
+from mHMM.src.transitions import TransitionModel
 from numpy.random import SeedSequence, default_rng
 import time 
 
